4239901b0b5fa107a66e59180f6d83be85155614
ragavkumarv <ragavkumarv@gmail.com>
2022-02-28 17:10:22 +0530

    getHeroes

--- controllers/hero.controller.js
+++ controllers/hero.controller.js
@@ -5,8 +5,14 @@ async function createHero(req, res) {
   res.status(201).json(createdHero);
 }
 
+async function getHeroes(req, res) {
+  const allHeroes = await HeroModel.find({});
+  res.status(200).json(allHeroes);
+}
+
 export const heroContoller = {
   createHero,
+  getHeroes,
 };
 
 // getHeroes -> unit + integeration
--- tests/unit/heroes.test.js
+++ tests/unit/heroes.test.js
@@ -2,9 +2,11 @@ import jest from "jest-mock";
 import httpMocks from "node-mocks-http";
 import { heroContoller } from "../../controllers/hero.controller.js";
 import { HeroModel } from "../../model/hero.model.js";
+import allHeroes from "../mock-data/all-heroes.json";
 import newHero from "../mock-data/new-hero.json";
 
 HeroModel.create = jest.fn(); // tracker chip
+HeroModel.find = jest.fn(); // tracker chip
 let req, res;
 
 beforeEach(() => {
@@ -12,25 +14,25 @@ beforeEach(() => {
   res = httpMocks.createResponse();
 });
 
-describe("Hero controller", () => {
+describe("Herocontroller.createHero", () => {
   beforeEach(() => {
     req.body = newHero;
   });
 
-  it("should have createTodo function", () => {
+  it("should have createHero function", () => {
     expect(typeof heroContoller.createHero).toBe("function");
-  });
-
-  it("should return 201 response code", async () => {
-    await heroContoller.createHero(req, res);
-    expect(res.statusCode).toBe(201);
-    expect(res._isEndCalled()).toBeTruthy();
   });
 
   it("should call HeroModel.create", async () => {
     await heroContoller.createHero(req, res);
     expect(HeroModel.create).toBeCalledWith(newHero);
     // HeroModel.create(newHero); -> Atlas
+  });
+
+  it("should return 201 response code", async () => {
+    await heroContoller.createHero(req, res);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
   });
 
   it("should call HeroModel.create", async () => {
@@ -44,3 +46,23 @@ describe("Hero controller", () => {
 //   const res = {
 //      json:  () => {}
 //   }
+
+describe("Herocontroller.getHeroes", () => {
+  it("should have getHeroes function", () => {
+    expect(typeof heroContoller.getHeroes).toBe("function");
+  });
+
+  it("should call HeroModel.find({})", async () => {
+    await heroContoller.getHeroes(req, res);
+    expect(HeroModel.find).toHaveBeenCalledWith({});
+  });
+
+  it("should return 200 response code and all Heroes", async () => {
+    HeroModel.find.mockReturnValue(allHeroes);
+    await heroContoller.getHeroes(req, res);
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled()).toBeTruthy();
+    expect(res._getJSONData()).toStrictEqual(allHeroes);
+    // Value provided by HeroModel(mock) -> Are we getting the same data in response?
+  });
+});
